fcd4ae897427b4e3018341d43d9541389f3a841f
selima <musaevas203@gmail.com>
2024-05-03 18:59:36 +0500

    add file

--- src/App/App.js
+++ src/App/App.js
@@ -3,10 +3,14 @@ import './App.css';
 
 import { Routes, Route, Link } from "react-router-dom";
 
+import Home from '../pages/Home/Home';
+import Support from '../pages/Support/Support'
+
 function App() {
   return (
     <div className="App">
-
+      <Home />
+      <Support />
     </div>
   );
 }
---
+++ src/pages/Support/Support.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Support.module.css'
+
+function Support() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Support
