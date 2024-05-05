f5ce1a6915f45ca35a3e910d901060da2217f834
unknown <wearw133@outlook.com>
2024-05-05 20:53:16 +0600

    header is ready

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-
 import { Routes, Route, Link } from "react-router-dom";
-
 import Home from '../pages/Home/Home';
 import Support from '../pages/Support/Support'
-
+import Header from '../pages/Header/Header'
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
       <Support />
     </div>
