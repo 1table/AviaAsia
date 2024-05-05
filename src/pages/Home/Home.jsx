3d2f944551d9916d006125aeb30d05cd3893bb31
unknown <wearw133@outlook.com>
2024-05-05 20:56:32 +0600

    Home is ready

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import './Home.module.css'
-
-function Home() {
+import './Main.css'
+const Home = () => {
   return (
-    <div>
-      
+    <div className='main'>
+      <div className="container">
+        <div className="main-block">
+          <div className="text-block">
+          <h1 className='first-text'>Дешёвые авиабилеты</h1>
+          <h3 className=' second-text'>Помогаем вам экономить</h3>
+          <div className="change-block">
+            <span className="change-span"><input type='text' className='change-input1'/></span>
+            <span className="change-span"><input type="text" className='change-input'/></span>
+            <span className="change-span"><input type="text" className='change-input'/></span>
+            <span className="change-span"><input type="text" className='change-input'/></span>
+            <span className="change-span"><input type="text" className='change-input'/></span>
+            <span className="change-span"><input type="text" className='change-input2'/></span>
+          </div>
+          <button className='btn'>Найти билеты</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
