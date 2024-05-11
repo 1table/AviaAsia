7484d836256f733a557d2dd3278b53efc0baf88a
unknown <wearw133@outlook.com>
2024-05-11 21:59:12 +0600

    start project

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import './App.css';
 
 import Home from './pages/Home/Home';
 import Support from './pages/Support/Support'
-
+import Header from './pages/Header/Header'
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
       <Home />
       <Support />
     </div>
