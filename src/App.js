d702b839ba2027b51d63439cdeae72bb460e84e0
selima <musaevas203@gmail.com>
2024-05-11 11:48:07 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { Routes, Route, Link } from "react-router-dom";
+// import { Routes, Route, Link } from "react-router-dom";
 
 import Home from './pages/Home/Home';
 import Support from './pages/Support/Support'
