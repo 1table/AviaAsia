f53a84869cf928eb8aa18cf3d8cc1d54da1cad37
unknown <wearw133@outlook.com>
2024-05-11 21:49:50 +0600

    checkout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Routes, Route, Link } from "react-router-dom";
 
 import Home from './pages/Home/Home';
 import Support from './pages/Support/Support'
+import Header from './pages/Header/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
       <Support />
     </div>
