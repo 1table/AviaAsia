8cd9d7cc9ba19a2df389983163d4f35e1d1ff020
unknown <wearw133@outlook.com>
2024-05-12 20:20:45 +0600

    header and home are ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './App.css';
 
-import { Routes, Route, Link } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 import Home from './pages/Home/Home';
 import Support from './pages/Support/Support'
 import Header from './pages/Header/Header'
+import About from './pages/About/About';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header />
-      <Home />
-      <Support />
+      <Routes>
+        <Route path ='/Home' element={<Home />} />
+        <Route path ='/About' element={<About />} />
+        <Route path ='/Support' element={<Support />} />
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
