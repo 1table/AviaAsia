b55f7a947a50c2d9dba1a8ce9d3fa7f74bf2948d
unknown <wearw133@outlook.com>
2024-05-22 19:50:55 +0600

    header

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
+
 const Header = () => {
   return (
-    <div className='header'>
-      <div className="container">
-        <div className="main-block">
-        <a href="" className="logo">logo</a>
-        <nav className="nav_link">
-            <Link to="Главная" className='nav_link-item'>Главная</Link>
-            <Link to="О нас" className='nav_link-item'>О нас</Link>
-            <Link to="Контакты" className='nav_link-item'>Контакты</Link>
-        </nav>
+    <div>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">logo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
         </div>
-      </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     </div>
   )
 }
