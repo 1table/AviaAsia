08d52d6e41b7aa50b6055b6afc5933c1990fc907
unknown <wearw133@outlook.com>
2024-05-24 22:33:39 +0600

    home ready

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,41 +4,43 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">logo</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">logo</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <Link to={'/Главная'} class="nav-link" >Главная</Link>
+              </li>
+              <li class="nav-item">
+                <Link to={'/О нас'} class="nav-link" >О нас</Link>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <Link to={'/Контакты'} class="nav-link" >Контакты</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <Link to={'/Главная'} class="nav-link" >Главная</Link>
-      </li>
-      <li class="nav-item">
-        <Link to={'/О нас'} class="nav-link" >О нас</Link>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <Link to={'/Контакты'} class="nav-link" >Контакты</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
     </div>
   )
 }
 
 export default Header
+
+
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-
+import './Home.css'
 const Home = () => {
   return (
-    <div>
-      
+    <div className='main'>
+      <div className="container">
+        <div className="main-block">
+          <div className="text-block">
+          <h1 className='first-text'>Дешёвые авиабилеты</h1>
+          <h3 className=' second-text'>Помогаем вам экономить</h3>
+          <div className="change-block">
+            <span className="change-span"><input type='search' className='change-input1' placeholder='откуда'  aria-label='search'/></span>
+            <span className="change-span"><input type="search" className='change-input'aria-label='search'  placeholder='куда'/></span>
+            <span className="change-span"><input type="text" className='change-input' placeholder='когда'/></span>
+            <span className="change-span"><input type="text" className='change-input' placeholder='обратно'/></span>
+            <span className="change-span"><input type="text" className='change-input2' placeholder=''/></span>
+          </div>
+          <button type="button" class="btn btn-primary btn-lg">Найти билет</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Home
-
