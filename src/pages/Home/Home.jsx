2d6a24e061ada3b7b88c8a9dd26d7a564d47befa
selima <musaevas203@gmail.com>
2024-05-31 20:43:54 +0500

    add first-section and second-section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,12 @@ import './Home.css'
 import { Link } from 'react-router-dom';
 // import '../../assets/search/search1'
 
-import RegistrationForm from '../../components/Registration/RegistrationForm';
-// import SearchForm from '../../components/SeachForm/SeachForm';
+// import Carousel from '../../components/Carousel/Carousel'
+
   
 const Home = () => {
   // const [inputValue, setInputValue] = useState('');
-  // const [inputValue2, setInputValue2] = useState('');
+  // const [inputValue2, setInputValue2] = useSКак мы работаем?tate('');
 
   // useEffect(() => {
   //   const spanValue = localStorage.getItem('spanValue');
@@ -27,19 +27,64 @@ const Home = () => {
   return (
     <div className='main'>
         <div className="main-block">
-          <div className="tex-block">
-          <h1 className='first-text'>Билеты по скидке</h1>
-          <h3 className=' second-text'>Помогаем вам экономить</h3>
-          <div className="change-block">
-            <RegistrationForm />
-            {/* <Link to={'/search'} className="change-span"><input type='search' value={inputValue} className='change-input1' placeholder='откуда'  aria-label='search'/></Link>
-            <Link to={'/search2'} className="change-span"><input type="search" value={inputValue2} className='change-input'aria-label='search'  placeholder='куда'/></Link>
-            <span className="change-span"><input type="text" className='change-input' placeholder='когда'/></span>
-            <span className="change-span"><input type="text" className='change-input' placeholder='обратно'/></span>
-            <span className="change-span"><input type="text" className='change-input2' placeholder=''/></span> */}
-          </div>
+          <section className='first-section'>
+           <h1 className='first-text'>Покупайте быстро и удобно
+           <br/> в любое время</h1>
+            
+          </section>
+          <section className='second-section'>
+            {/* <Carousel /> */}
+          </section>
+            <section className="third-section">
+              <h2>Как мы работаем?</h2>
+              <div className='card-container'>
+                <div className='card-section'>
+                  <h3> Шаг 1 </h3>
+                  <h4>Заполните форму регистрации</h4>
+                  <p> 
+                     Заполните формы поиска авиабилетов (направления,
+                     даты, количество пассажиров)
+                  </p>
+                </div>
+                <div className='card-section'>
+                  <h3> Шаг 2 </h3>
+                  <h4>Выберите время прибытия и отправления</h4>
+                  <p>
+                      Выберите оптимальное
+                      время вылета и прибытия
+                  </p>
+
+                </div> 
+              </div>
+              <div className='card-container'>
+                <div className='card-section'>
+                  <h3> Шаг 3 </h3>
+                  <h4>Купите билет</h4>
+                  <p> 
+                    Заполните данные пассажиров в строгом соответствии с
+                    паспортом. Сделав выбор нажмите кнопку “Купить билет”
+                  </p>
+                </div>
+                <div className='card-section'>
+                  <h3>Шаг 4</h3>
+                  <h4>Готово! Собирайте чемоданы</h4>
+                  <p> 
+                  После покупки авиабилетов, вы можете узнать у
+                  представителя авиакомпании о необходимости
+                  распечатанного авиабилета и распечатать его
+                  </p>
+                </div>          
+              </div>
+
+
+              {/* <RegistrationForm /> */}
+              {/* <Link to={'/search'} className="change-span"><input type='search' value={inputValue} className='change-input1' placeholder='откуда'  aria-label='search'/></Link>
+              <Link to={'/search2'} className="change-span"><input type="search" value={inputValue2} className='change-input'aria-label='search'  placeholder='куда'/></Link>
+              <span className="change-span"><input type="text" className='change-input' placeholder='когда'/></span>
+              <span className="change-span"><input type="text" className='change-input' placeholder='обратно'/></span>
+              <span className="change-span"><input type="text" className='change-input2' placeholder=''/></span> */}
+            </section>
           {/* <button type="button" class="btn btn-primary btn-lg">Найти билет</button> */}
-          </div>
         </div>
     </div>
   )
